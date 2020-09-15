a76854e03ac4e743acef28b1e4e03581affe68b7
Jossanl <71289092+Jossanl@users.noreply.github.com>
2020-09-15 13:21:19 +0200

    SchoolProject

---
+++ Assets/SCRIPTS 1/Enemy_Killbox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Killbox : MonoBehaviour {
+
+    GameObject gameObjectToKill;
+
+    private void Start() {
+
+        gameObjectToKill = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag ("Player") == true) {
+            if(collision.gameObject.GetComponent<PlayerMovement>().IsFalling() == true) {
+                gameObject.GetComponentInParent<Enemy_GreenSlimeMovement>().KillMe();
+        
+            }
+        }
+    }
+}
